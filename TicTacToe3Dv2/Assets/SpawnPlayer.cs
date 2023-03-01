f33717e783467142f38962956d34965f1d76830c
lasiest <valenstiowiyogo@gmail.com>
2023-03-01 18:14:26 +0700

    player 1 dan player 2 masuk ke scene game
    -Error setiap player bisa memulai permainan harusnya hanya yang create yang bisa memulai permainan

---
+++ TicTacToe3Dv2/Assets/SpawnPlayer.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+[RequireComponent(typeof(PhotonView))]
+public class SpawnPlayer : MonoBehaviourPun
+{
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject StartButton;
+    public TMP_Text Infotext;
+    public bool flag = false;
+    // public GameObject player1InfoText;
+    // public GameObject player2InfoText;
+
+    private void Awake() {
+        player1.SetActive(false);
+        player2.SetActive(false);
+        // player1InfoText.SetActive(false);
+        // player2InfoText.SetActive(false);     
+        StartButton.SetActive(false);
+        RPC_SetActivePlayer();   
+    }
+
+    [PunRPC]
+    private void SetActivePlayers() {
+        if(!player1.activeInHierarchy){
+            player1.SetActive(true);
+            Debug.Log("set active player 1");
+            Infotext.text = "Waiting for other player";
+            // player1InfoText.SetActive(true);
+        }else if(player1.activeInHierarchy && !player2.activeInHierarchy){
+            player2.SetActive(true);
+            Debug.Log("set active player 2");
+            Infotext.text = "Waiting for host";
+            // player1InfoText.SetActive(false);
+            // player2InfoText.SetActive(true);
+        }
+    }
+
+    private void RPC_SetActivePlayer(){
+        photonView.RPC(nameof(SetActivePlayers), RpcTarget.AllBuffered);
+        Debug.Log("RPC_SetActivePlayer terpanggil");
+    }
+
+    private void Update() {
+        if(player1.activeInHierarchy && player2.activeInHierarchy){
+            StartButton.SetActive(true);
+        }
+    }
+
+    public void StartFunction(){
+
+    }
+}
