3dfb3732118f215d3916c27f499658739d2b95f0
lasiest <valenstiowiyogo@gmail.com>
2023-03-02 15:41:54 +0700

    Done

--- TicTacToe3Dv2_clone_0/Assets/SpawnPlayer.cs
+++ TicTacToe3Dv2_clone_0/Assets/SpawnPlayer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(PhotonView))]
 public class SpawnPlayer : MonoBehaviourPun
@@ -10,14 +11,18 @@ public class SpawnPlayer : MonoBehaviourPun
     public GameObject player1;
     public GameObject player2;
     public GameObject StartButton;
-    public GameObject _lingkaran;
-    public GameObject _kotak;
+    public GameObject MenuButton;
+    public GameObject lingkaran;
+    public GameObject silang;
     public GameObject board;
     public TMP_Text Infotext;
     public bool gameStarted;
     public bool gameEnded;
     public int turn;
     public int[] values;
+    public int[] flag;
+    public bool xWin;
+    public bool OWin;
 
     private void Awake() {
         player1.SetActive(false);
@@ -28,7 +33,9 @@ public class SpawnPlayer : MonoBehaviourPun
         turn = 0;
         gameStarted = false;
         gameEnded = false;
-
+        MenuButton.SetActive(false);
+        xWin = false;
+        OWin = false;
     }
 
     [PunRPC]
@@ -49,25 +56,40 @@ public class SpawnPlayer : MonoBehaviourPun
         Debug.Log("RPC_SetActivePlayer terpanggil");
     }
 
-    private void RPC_SetActiveStartButton(){
-        photonView.RPC(nameof(SetActivePlayers), RpcTarget.Others);
-    }
+    // private void RPC_SetActiveStartButton(){
+    //     photonView.RPC(nameof(SetActivePlayers), RpcTarget.Others);
+    // }
 
     private void Update() {
         if(player1.activeInHierarchy && player2.activeInHierarchy && photonView.IsMine && !gameStarted){
             Infotext.text = "Start when you ready!";
             StartButton.SetActive(true);
         }
-        if(gameStarted && photonView.IsMine && turn % 2 == 0){
+        if(gameStarted && photonView.IsMine && turn % 2 == 0 && !gameEnded){
             Infotext.text = "YourTurn";
         }else if(gameStarted && photonView.IsMine && turn % 2 != 0){
             Infotext.text = "Wait for your turn";
         }
 
-        if(gameStarted && !photonView.IsMine && turn % 2 == 0){
+        if(gameStarted && !photonView.IsMine && turn % 2 == 0 && !gameEnded){
             Infotext.text = "Wait for your turn";
         }else if(gameStarted && !photonView.IsMine && turn % 2 != 0){
             Infotext.text = "YourTurn";
+        }
+        xWinCondition();
+        oWinCondition();            
+
+        if(gameEnded){
+            if(xWin){
+                Infotext.text = "Player 1 Win the Game";
+            }else if(OWin){
+                Infotext.text = "Player 2 Win the Game";
+            }
+            MenuButton.SetActive(true);
+        }
+        if(turn > 8){
+            Infotext.text = "Tie";
+            MenuButton.SetActive(true);
         }
     }
 
@@ -88,16 +110,139 @@ public class SpawnPlayer : MonoBehaviourPun
         if(values[value] == 0){
             values[value] = (turn % 2) + 1;
             turn++;
-        }           
+        }
+        // xWinCondition();
+        // oWinCondition();     
     }
-
+    
     public void RPC_buttonIsClick(int value){
-        if(photonView.IsMine && turn % 2 == 0){
+        if(photonView.IsMine && turn % 2 == 0 && !gameEnded){
             photonView.RPC(nameof(buttonIsClick), RpcTarget.All, value);
-        }else if(!photonView.IsMine && turn % 2 != 0){
+            InstantiateObject();
+        }else if(!photonView.IsMine && turn % 2 != 0 && !gameEnded){
             photonView.RPC(nameof(buttonIsClick), RpcTarget.All, value);
+            InstantiateObject();
         }else{
             Debug.Log("Not your turn");
+        }
+    }   
+
+    public void backToMainMenu(){
+        SceneManager.LoadScene("Lobby");
+        PhotonNetwork.LeaveRoom();
+    }
+
+        public void xWinCondition(){
+        if(values[0] == 1 && values[1] == 1 && values[2] == 1){
+            Debug.Log("X win"); 
+            gameEnded = true;
+            xWin = true;
+        }else if(values[3] == 1 && values[4] == 1 && values[5] == 1){
+            Debug.Log("X win"); 
+            gameEnded = true;
+            xWin = true;
+        }else if(values[6] == 1 && values[7] == 1 && values[8] == 1){
+            Debug.Log("X win"); 
+            gameEnded = true;
+            xWin = true;
+        }else if(values[0] == 1 && values[3] == 1 && values[6] == 1){
+            Debug.Log("X win"); 
+            gameEnded = true;
+            xWin = true;
+        }else if(values[1] == 1 && values[4] == 1 && values[7] == 1){
+            Debug.Log("X win"); 
+            gameEnded = true;
+            xWin = true;
+        }else if(values[2] == 1 && values[5] == 1 && values[8] == 1){
+            Debug.Log("X win"); 
+            gameEnded = true;
+            xWin = true;
+        }else if(values[0] == 1 && values[4] == 1 && values[8] == 1){
+            Debug.Log("X win"); 
+            gameEnded = true;
+            xWin = true;
+        }else if(values[2] == 1 && values[4] == 1 && values[6] == 1){
+            Debug.Log("X win"); 
+            gameEnded = true;
+            xWin = true;
+        }
+    }
+
+    public void oWinCondition(){
+        if(values[0] == 2 && values[1] == 2 && values[2] == 2){
+            Debug.Log("O win"); 
+            gameEnded = true;
+            OWin = true;
+        }else if(values[3] == 2 && values[4] == 2 && values[5] == 2){
+            Debug.Log("O win"); 
+            gameEnded = true;
+            OWin = true;
+        }else if(values[6] == 2 && values[7] == 2 && values[8] == 2){
+            Debug.Log("O win"); 
+            gameEnded = true;
+            OWin = true;
+        }else if(values[0] == 2 && values[3] == 2 && values[6] == 2){
+            Debug.Log("O win"); 
+            gameEnded = true;
+            OWin = true;
+        }else if(values[1] == 2 && values[4] == 2 && values[7] == 2){
+            Debug.Log("O win"); 
+            gameEnded = true;
+            OWin = true;
+        }else if(values[2] == 2 && values[5] == 2 && values[8] == 2){
+            Debug.Log("O win"); 
+            gameEnded = true;
+            OWin = true;
+        }else if(values[0] == 2 && values[4] == 2 && values[8] == 2){
+            Debug.Log("O win"); 
+            gameEnded = true;
+            OWin = true;
+        }else if(values[2] == 2 && values[4] == 2 && values[6] == 2){
+            Debug.Log("O win"); 
+            gameEnded = true;
+            OWin = true;
+        }        
+    }
+
+    private void InstantiateObject(){
+        if(values[0] == 1 && flag[0] != 1){
+            PhotonNetwork.Instantiate(lingkaran.name, new Vector3(-4f, 1.3f, 4f), Quaternion.identity); flag[0] = 1;
+        }else if(values[1] == 1 && flag[1] != 1){
+            PhotonNetwork.Instantiate(lingkaran.name, new Vector3(0f, 1.3f, 4f), Quaternion.identity); flag[1] = 1;
+        }else if(values[2] == 1 && flag[2] != 1){
+            PhotonNetwork.Instantiate(lingkaran.name, new Vector3(4f, 1.3f, 4f), Quaternion.identity); flag[2] = 1;
+        }else if(values[3] == 1 && flag[3] != 1){
+            PhotonNetwork.Instantiate(lingkaran.name, new Vector3(-4f, 1.3f, 0f), Quaternion.identity); flag[3] = 1;
+        }else if(values[4] == 1 && flag[4] != 1){
+            PhotonNetwork.Instantiate(lingkaran.name, new Vector3(0f, 1.3f, 0f), Quaternion.identity); flag[4] = 1;
+        }else if(values[5] == 1 && flag[5] != 1){
+            PhotonNetwork.Instantiate(lingkaran.name, new Vector3(4f, 1.3f, 0f), Quaternion.identity); flag[5] = 1;
+        }else if(values[6] == 1 && flag[6] != 1){
+            PhotonNetwork.Instantiate(lingkaran.name, new Vector3(-4f, 1.3f, -4f), Quaternion.identity); flag[6] = 1;
+        }else if(values[7] == 1 && flag[7] != 1){
+            PhotonNetwork.Instantiate(lingkaran.name, new Vector3(0f, 1.3f, -4f), Quaternion.identity); flag[7] = 1;
+        }else if(values[8] == 1 && flag[8] != 1){
+            PhotonNetwork.Instantiate(lingkaran.name, new Vector3(4f, 1.3f, -4f), Quaternion.identity); flag[8] = 1;
+        }
+
+        if(values[0] == 2 && flag[0] != 1){
+            PhotonNetwork.Instantiate(silang.name, new Vector3(-4f, 1.3f, 4f), Quaternion.identity); flag[0] = 1;
+        }else if(values[1] == 2 && flag[1] != 1){
+            PhotonNetwork.Instantiate(silang.name, new Vector3(0f, 1.3f, 4f), Quaternion.identity); flag[1] = 1;
+        }else if(values[2] == 2 && flag[2] != 1){
+            PhotonNetwork.Instantiate(silang.name, new Vector3(4f, 1.3f, 4f), Quaternion.identity); flag[2] = 1;
+        }else if(values[3] == 2 && flag[3] != 1){
+            PhotonNetwork.Instantiate(silang.name, new Vector3(-4f, 1.3f, 0f), Quaternion.identity); flag[3] = 1;
+        }else if(values[4] == 2 && flag[4] != 1){
+            PhotonNetwork.Instantiate(silang.name, new Vector3(0f, 1.3f, 0f), Quaternion.identity); flag[4] = 1;
+        }else if(values[5] == 2 && flag[5] != 1){
+            PhotonNetwork.Instantiate(silang.name, new Vector3(4f, 1.3f, 0f), Quaternion.identity); flag[5] = 1;
+        }else if(values[6] == 2 && flag[6] != 1){
+            PhotonNetwork.Instantiate(silang.name, new Vector3(-4f, 1.3f, -4f), Quaternion.identity); flag[6] = 1;
+        }else if(values[7] == 2 && flag[7] != 1){
+            PhotonNetwork.Instantiate(silang.name, new Vector3(0f, 1.3f, -4f), Quaternion.identity); flag[7] = 1;
+        }else if(values[8] == 2 && flag[8] != 1){
+            PhotonNetwork.Instantiate(silang.name, new Vector3(4f, 1.3f, -4f), Quaternion.identity); flag[8] = 1;
         }
     }
 }
--- TicTacToe3Dv2_clone_0/Assets/isClicked.cs
+++ TicTacToe3Dv2_clone_0/Assets/isClicked.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class isClicked : MonoBehaviour
 {   
@@ -10,5 +11,8 @@ public class isClicked : MonoBehaviour
         Debug.Log(value);
         // spawnPlayer.buttonIsClick(value);
         spawnPlayer.RPC_buttonIsClick(value);
+        // if(spawnPlayer.turn % 2 == 0){
+        //     Instantiate
+        // }
     }
 }
